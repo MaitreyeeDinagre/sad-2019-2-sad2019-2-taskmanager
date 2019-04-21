ac27a0cfafffca5c4d756fe4f40cbd8c94bb6bd1
mubi77 <mubeenmirza000@gmail.com>
2019-04-21 02:52:18 +0100

    created web services for EPIC

--- TaskManager/src/main/java/com/srhheidelberg/sad/taskmanager/DAOInterface/EpicDAOInterface.java
+++ TaskManager/src/main/java/com/srhheidelberg/sad/taskmanager/DAOInterface/EpicDAOInterface.java
@@ -1,7 +1,19 @@
 package com.srhheidelberg.sad.taskmanager.DAOInterface;
 
+import java.util.List;
+
+
+import com.srhheidelberg.sad.taskmanager.model.Epic;
+
 public interface EpicDAOInterface {
 	
+	List<Epic> getAllEpic();
+	
+	Epic getEpic(Integer EpicId);
+	
+	Epic createEpic(Epic epic);
+	
+
 	
 
 }
