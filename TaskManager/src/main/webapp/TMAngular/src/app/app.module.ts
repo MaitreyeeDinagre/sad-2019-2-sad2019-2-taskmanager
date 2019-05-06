dc589d56fc3287e38066a3dd620aac07c8fe60dd
kaustavdutta1989 <kaustavpo12@gmail.com>
2019-05-06 18:35:08 +0200

    app.modue.ts
    Reloaded

---
+++ TaskManager/src/main/webapp/TMAngular/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterModule } from './footer/footer.module';
+import { HeaderModule } from './header/header.module';
+import { LoginComponent } from './login/login.component';
+import { LoginModule } from './login/login.module';
+import { ManagerviewModule } from './managerview/managerview.module';
+import { CommonviewModule } from './commonview/commonview.module';
+import { TasklogModule } from './tasklog/tasklog.module';
+import { EmployeeserviceService } from './employeeservice.service';
+import { CookieService } from 'ngx-cookie-service/cookie-service/cookie.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    FooterModule,
+    HeaderModule,
+    LoginModule,
+    ManagerviewModule,
+    CommonviewModule,
+    TasklogModule
+  ],
+  providers: [EmployeeserviceService, CookieService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
