5fc4a53ac910c0f11d4515d6f8e11db1c0dbe85e
nivedithabr <nivedithabr@gmail.com>
2019-05-07 14:54:09 +0200

    Updated Epic Service to Task Service

--- TaskManager/src/main/webapp/TMAngular/src/app/task.service.spec.ts
+++ TaskManager/src/main/webapp/TMAngular/src/app/task.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { TaskService } from './task.service';
 
-describe('EpicService', () => {
+describe('TaskService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
