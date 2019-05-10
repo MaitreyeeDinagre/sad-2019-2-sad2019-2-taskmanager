13ba61a6836123d24e5b793c3867ea2738b59f2e
kaustavdutta1989 <kaustavpo12@gmail.com>
2019-05-10 09:42:12 +0200

    CRUD End-End
    Co-Authored-By: sunny-221430 <sunny-221430@users.noreply.github.com>
    Co-Authored-By: arpithabasavraj <arpithabasavraj@users.noreply.github.com>
    Co-Authored-By: MAITREYEE DINGARE <maitreyeedinagre@users.noreply.github.com>
    Co-Authored-By: Mubeen <mubeen777@users.noreply.github.com>
    Co-Authored-By: Manzil Chakraborti <manzil94@users.noreply.github.com>
    Co-Authored-By: nivedithabr <nivedithabr@users.noreply.github.com>

--- TaskManager/src/main/webapp/TMAngular/src/app/employees/employeelistview/employeelistview.component.ts
+++ TaskManager/src/main/webapp/TMAngular/src/app/employees/employeelistview/employeelistview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/employee.service';
 import { issue } from 'src/app/issue.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-employeelistview',
@@ -9,23 +10,42 @@ import { issue } from 'src/app/issue.model';
 })
 export class EmployeelistviewComponent implements OnInit {
 
-  constructor(private empService : EmployeeService) { }
+  constructor(private empService : EmployeeService, private httpclient: HttpClient) { }
 
+  selectedEmployeId : number;
+  employees : any;
   ngOnInit() {
+    this.presets();
   }
 
-  deleteEmployee(empId : number) {
-    this.empService.deleteEmployee(empId)
-    .subscribe(Response => {
-      //Success Mesaage
-      console.log("DEleted");
-    });
+  deleteEmployee() {
+    if(this.selectedEmployeId) {
+      this.empService.deleteEmployee(this.selectedEmployeId)
+      .subscribe(Response => {
+        alert("Deleted");
+        //Success Mesaage
+        console.log("Deleted");
+      });
+    }
+    else {
+      alert("Select Employee");
+    }
+      
   }
 
   updateIssue(issue : issue) {
-    this.empService.updateIssue(issue)
+    this.empService.updateIssue(null)
     .subscribe(Response => {
       console.log("Updated");
     });
+  }
+
+  presets() {
+    this.httpclient
+  .get('http://localhost:8888/JIRA-lite/TaskManager/issue/')
+  .subscribe( response => {
+    this.employees = response;
+    console.log(response);
+  });
   }
 }
